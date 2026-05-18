d7a52c5713bca4905f1b30d969fa00789b9fa459
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-05-18 17:55:24 +0100

    working with 'nio'  and making new file with it then right 'hello world'

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 //import java.io.File;
 //import java.io.FileWriter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+//import java.io.FileInputStream;
+//import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 
 public class Main {
     public static void main(String[] args){
-        try(FileInputStream input = new FileInputStream("coffe.jpg");
-            FileOutputStream output = new FileOutputStream("copy.jpg")) {
-            int i;
-            while((i = input.read()) != -1){
-                output.write(i);
-            }
-            System.out.println("File copied successfully.");
+        Path path = Paths.get("hi.txt");
+        String data = "hello world!";
+        try{
+            Files.write(path, data.getBytes(), StandardOpenOption.CREATE);
         } catch (IOException e){
             System.out.println("File reading error.");
             e.printStackTrace();
