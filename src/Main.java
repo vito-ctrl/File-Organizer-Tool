ec7fe211b313507b9512c4db4fa728626f68e2f9
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-05-18 02:19:40 +0100

    copy a file image to another file using io

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,34 @@
 //import java.io.File;
 //import java.io.FileWriter;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args){
-        try(FileInputStream input = new FileInputStream("test.txt")) {
+        try(FileInputStream input = new FileInputStream("coffe.jpg");
+            FileOutputStream output = new FileOutputStream("copy.jpg")) {
             int i;
-            while ((i = input.read()) != -1){
-                System.out.print((char) i);
+            while((i = input.read()) != -1){
+                output.write(i);
             }
-            System.out.print("\nreading file succesfully\n");
+            System.out.println("File copied successfully.");
         } catch (IOException e){
             System.out.println("File reading error.");
             e.printStackTrace();
         }
     }
 }
+
+//        try(FileInputStream input = new FileInputStream("test.txt")) {
+//            int i;
+//            while ((i = input.read()) != -1){
+//                System.out.print((char) i);
+//            }
+//            System.out.print("\nreading file succesfully\n");
+
+
+
 //            File myOb = new File("test.txt");
 //            if(myOb.exists()){
 //                System.out.println(myOb.getName() + " exists :)");
